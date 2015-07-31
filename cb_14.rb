7b48e007de725e868ff9a4051c5c0be708ebc713
EuyJin Park <euyjin0406@korea.ac.kr>
2015-07-31 20:07:00 +0900

    Create cb_14.rb

---
+++ cb_14.rb
@@ -0,0 +1,13 @@
+def TimeConvert(num)
+
+  # code goes here
+  h = num/60
+  m = num-h*60
+  
+  return "#{h}:#{m}"
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+TimeConvert(STDIN.gets)           
