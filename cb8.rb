814b937ee22236e0402cb9fd12357b7ffa8b619c
EuyJin Park <euyjin0406@korea.ac.kr>
2015-07-31 17:57:26 +0900

    Create cb8.rb

---
+++ cb8.rb
@@ -0,0 +1,11 @@
+def AlphabetSoup(str)
+
+  # code goes here
+  a=str.split("").sort
+  return a.join
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+AlphabetSoup(STDIN.gets)           
