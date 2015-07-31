d844a0f62c992acc097ef4bdbf0b8157825269ee
EuyJin Park <euyjin0406@korea.ac.kr>
2015-07-31 18:24:49 +0900

    Create cb10.rb

---
+++ cb10.rb
@@ -0,0 +1,10 @@
+def WordCount(str)
+
+  a=str.split(" ")
+  return a.length
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+WordCount(STDIN.gets)           
